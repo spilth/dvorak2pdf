0c4e224e2edd4689061081941a6f74b34fce219a
Brian Kelly <polymonic@gmail.com>
2014-02-17 00:11:17 -0500

    Adds descriptions, abilities, flavor text and tokens.

--- cards.rb
+++ cards.rb
@@ -25,6 +25,30 @@ Prawn::Document.generate('cards.pdf', page_size: [2.75 * 72, 3.75 * 72], margin:
       text_box card['type'], at: [9, CARD_HEIGHT - 27]
     end
 
+    if card['description']
+      font "Helvetica", style: :normal, size: 8 do
+        text_box card['description'], at: [9, CARD_HEIGHT - 45]
+      end
+    end
+
+    if card['tokens']
+      font "Helvetica", style: :normal, size: 8 do
+        text_box "When this card is put into play, place #{card['tokens']} tokens on it.", at: [9, CARD_HEIGHT - 90]
+      end
+    end
+
+    if card['ability']
+      font "Helvetica", style: :normal, size: 8 do
+        text_box card['ability'], at: [9, CARD_HEIGHT - 135]
+      end
+    end
+
+    if card['flavor']
+      font "Helvetica", style: :italic, size: 8 do
+        text_box card['flavor'], at: [9, CARD_HEIGHT - 180]
+      end
+    end
+
     on_first_page = false if on_first_page
 
   end
